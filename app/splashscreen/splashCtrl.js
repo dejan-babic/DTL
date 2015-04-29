5ce2802ae7acc4d12a1523e7b320ab2d8fdbb191
babicaja <babicaja@gmail.com>
2015-04-30 00:55:00 +0200

    start of splashscreen feature

---
+++ app/splashscreen/splashCtrl.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Do basic system check before going to home screen
+ */
+dtl.controller('splashCtrl', function(){
+
+	var vm = this;
+
+	function init () {
+		vm.initMessage = "Loading from here";
+	}
+	
+	init();
+
+});
