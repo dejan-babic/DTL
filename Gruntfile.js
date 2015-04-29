d131b8ba2484830ac76156d0aca18b9975690959
babicaja <babicaja@gmail.com>
2015-04-29 22:50:14 +0200

    less support

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 			local: ['dist']
 		},
 		copy: {
-			local: {
+			buildLocal: {
 				files: [
 					{ expand: true, cwd: '', src: ['app/**', 'templates/**'], dest: 'dist/local' },
 					{ expand: true, cwd: 'libs', src: ['angular/angular.js'], dest: 'dist/local/libs' },
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
 					{ expand: true, cwd: 'libs', src: ['bootstrap/dist/js/bootstrap.js'], dest: 'dist/local/libs' },
 					{ expand: true, cwd: 'libs', src: ['bootstrap/dist/css/bootstrap.css'], dest: 'dist/local/libs' },
 					{ expand: true, cwd: 'libs', src: ['ng-device-detector/ng-device-detector.js'], dest: 'dist/local/libs' },
+					{ expand: true, cwd: 'css', src: ['dtl-main.css'], dest: 'dist/local/css' },
 					{ expand: true, cwd: '', src: ['index.html'], dest: 'dist/local' },
 				]
 			},
@@ -27,6 +28,13 @@ module.exports = function (grunt) {
 				files: [
 					{ expand: true, cwd: 'dist/local', src: ['**'], dest: '../../wamp/www/DTL' },
 				]
+			}
+		},
+		less: {
+			local: {
+				files: {
+					'css/dtl-main.css': 'css/dtl-main.less'
+				}
 			}
 		}
 	});
@@ -38,6 +46,6 @@ module.exports = function (grunt) {
 		});
 	});
 
-	grunt.registerTask('local', ['clean:local', 'mkdirs:local', 'copy:local', 'copy:deployLocal']);
+	grunt.registerTask('local', ['less:local', 'clean:local', 'mkdirs:local', 'copy:buildLocal', 'copy:deployLocal']);
 
 };
