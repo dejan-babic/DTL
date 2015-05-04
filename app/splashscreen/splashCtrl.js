1a77771f4a5df3875b2d209d3c42124bf97c7e3d
Dejan Babić <dejan.babic@devtechgroup.com>
2015-05-05 01:24:45 +0200

    initial chained promises for system checks

--- app/splashscreen/splashCtrl.js
+++ app/splashscreen/splashCtrl.js
@@ -3,14 +3,55 @@
 /**
  * Do basic system check before going to home screen
  */
-dtl.controller('splashCtrl', function(){
+dtl.controller('splashCtrl', function($log, $q, deviceDetector){
+	//TODO [DB] Get all values from config/labels
 
 	var vm = this;
 
-	function init () {
-		vm.initMessage = "Loading from here";
+	var SYS_ERROR = {
+		BROWSER_SUPPORT: "Browser in not supported"
+	};
+
+	var INIT_CHECK = {
+		BROWSER_CHECK: "Checking browser compatibility",
+		CONN_CHECK: "Testing services"
+	};
+
+	// Labels flags and helpers
+	vm.initMessage = "Preparing DevTech Library";
+
+	function setInitMessage (msg) {
+		vm.initMessage = msg;
 	}
 
-	init();
+	function checkClientAgent () {
+		$log.debug("splashCtrl:checkClientAgent()");
+		setInitMessage(INIT_CHECK.BROWSER_CHECK);
+		var deferred = $q.defer();
+		deviceDetector.browser === "chrome" ? deferred.resolve() : deferred.reject(SYS_ERROR.BROWSER_SUPPORT);
+		return deferred.promise;
+	}
 
+	function checkConnectivity () {
+		$log.debug("splashCtrl:checkConnectivity()");
+		setInitMessage(INIT_CHECK.CONN_CHECK);
+		var deferred = $q.defer();
+		return deferred.promise;
+	}
+
+	function handleSystemCheckFail (reason) {
+		$log.debug("splashCtrl:handleSystemCheckFail()");
+		$log.debug(reason);
+	}
+
+	// Goes through check procedures
+	function init () {
+		$log.debug("splashCtrl:init()");
+		checkClientAgent()
+			.then(checkConnectivity)
+			.catch(handleSystemCheckFail)
+	}
+
+	// Start the show
+	init();
 });
