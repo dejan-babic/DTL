7b5f10cb4015be67b332a21b4650ef40fe6803db
Dejan Babić <dejan.babic@devtechgroup.com>
2015-05-12 00:20:07 +0200

    basic test coverage

---
+++ app/core/controllers/mainCtrl.spec.js
@@ -0,0 +1,21 @@
+describe('mainCtrl', function() {
+
+	var $rootScope, $scope, $controller;
+
+	beforeEach(module('dtl'));
+
+	beforeEach(inject(function(_$rootScope_, _$controller_){
+
+		$rootScope = _$rootScope_;
+		$scope = $rootScope.$new();
+		$controller = _$controller_;
+
+		mainCtrl = $controller('mainCtrl', {'$rootScope' : $rootScope, '$scope': $scope});
+
+	}));
+
+	it('should exist', function() {
+		expect(mainCtrl).toBeDefined();
+	});
+
+});
