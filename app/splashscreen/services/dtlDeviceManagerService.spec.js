82eae50bfb7c940ab833bd93aa33e50c961a3ddf
Dejan Babić <dejan.babic@devtechgroup.com>
2015-05-20 21:38:37 +0200

    start of tests for service

--- app/splashscreen/services/dtlDeviceManagerService.spec.js
+++ app/splashscreen/services/dtlDeviceManagerService.spec.js
@@ -1,14 +1,15 @@
 describe('dtlDeviceManagerService', function() {
 
-	var $rootScope, $scope, dtlDeviceManager;
+	var $rootScope, $scope, dtlDeviceManager, SYSTEM_MESSAGE;
 
 	//noinspection JSUnresolvedFunction
 	beforeEach(module('dtl'));
 	//noinspection JSUnresolvedFunction
-	beforeEach(inject(function(_$rootScope_, _dtlDeviceManager_){
+	beforeEach(inject(function(_$rootScope_, _dtlDeviceManager_, _SYSTEM_MESSAGE_){
 		$rootScope = _$rootScope_;
 		$scope = $rootScope.$new();
 		dtlDeviceManager = _dtlDeviceManager_;
+		SYSTEM_MESSAGE = _SYSTEM_MESSAGE_;
 	}));
 
 	it('should expose an checkDevice method', function() {
@@ -16,9 +17,17 @@ describe('dtlDeviceManagerService', function() {
 		expect(dtlDeviceManager.checkDevice).toBeDefined();
 	});
 
+	describe('checkDevice', function() {
+		//TODO [DB] figure out how to test the promise
+	});
+
 	it('should expose an checkUserAgent method', function() {
 		//noinspection JSCheckFunctionSignatures
 		expect(dtlDeviceManager.checkUserAgent).toBeDefined();
+	});
+
+	describe('checkUserAgent', function() {
+		//TODO [DB] figure out how to test the promise
 	});
 
 });
