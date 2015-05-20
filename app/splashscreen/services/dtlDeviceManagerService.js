6ffe38011b245e2486c8d15a18be86b4377ac03b
Dejan Babić <dejan.babic@devtechgroup.com>
2015-05-20 21:38:21 +0200

    removed string messages

--- app/splashscreen/services/dtlDeviceManagerService.js
+++ app/splashscreen/services/dtlDeviceManagerService.js
@@ -7,9 +7,9 @@
 	angular.module('dtl').
 		factory('dtlDeviceManager', dtlDeviceManager);
 
-	dtlDeviceManager.$inject = ['$log', '$q', 'deviceDetector'];
+	dtlDeviceManager.$inject = ['$log', '$q', 'deviceDetector', 'SYSTEM_MESSAGE'];
 
-	function dtlDeviceManager($log, $q, deviceDetector) {
+	function dtlDeviceManager($log, $q, deviceDetector, SYSTEM_MESSAGE) {
 
 		return {
 			checkDevice: checkDevice,
@@ -20,9 +20,9 @@
 			$log.debug('dtlDeviceManager:checkDevice()');
 			var deferred = $q.defer();
 			if (deviceDetector.isDesktop()) {
-				deferred.resolve()
+				deferred.resolve(SYSTEM_MESSAGE.ACTION_OK)
 			} else {
-				deferred.reject("Only Desktop is supported for now"); //TODO [DB] Move msg to SYS ERRORS
+				deferred.reject(SYSTEM_MESSAGE.DEVICE_NOT_SUPPORTED);
 			}
 
 			return deferred.promise;
@@ -31,7 +31,7 @@
 		function checkUserAgent() {
 			$log.debug('dtlDeviceManager:checkUserAgent()');
 			var deferred = $q.defer();
-			deviceDetector.browser === "chrome" ? deferred.resolve() : deferred.reject('Browser not supported'); //TODO [DB] Move msg to SYS ERRORS
+			deviceDetector.browser === "chrome" ? deferred.resolve(SYSTEM_MESSAGE.ACTION_OK) : deferred.reject(SYSTEM_MESSAGE.BROWSER_NOT_SUPPORTED);
 			return deferred.promise;
 		}
 	}
