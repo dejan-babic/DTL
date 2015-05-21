bf1221c1056653d6ef5796ca7ac387f209af00b0
Dejan Babić <dejan.babic@devtechgroup.com>
2015-05-21 22:01:24 +0200

    UI blocker on route changes

--- app/core/controllers/mainCtrl.js
+++ app/core/controllers/mainCtrl.js
@@ -7,10 +7,29 @@
 	angular.module('dtl')
 		.controller('mainCtrl', mainCtrl);
 
-	mainCtrl.$inject = ['$scope'];
+	mainCtrl.$inject = ['$rootScope', 'dtlSpinner'];
 
-	function mainCtrl() {
+	function mainCtrl($rootScope, dtlSpinner) {
+
 		var vm = this;
+
+		vm.init = function () {
+			registerListeners();
+		};
+
+		vm.init();
+
+		function registerListeners() {
+
+			$rootScope.$on("$routeChangeStart", function(event, next, current) {
+				dtlSpinner.start();
+			});
+			$rootScope.$on("$routeChangeSuccess", function(event, next, current) {
+				dtlSpinner.stop();
+			});
+
+		}
+
 	}
 
 })();
