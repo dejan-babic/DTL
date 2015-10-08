d6a7eb2cec4842c30d1de71eca0bb6e3980433e9
dejan-babic <dejan.babic@devtechgroup.com>
2015-10-08 18:19:57 +0200

    changed config values to test fallback procedures

--- app/core/modules/dtlConfig/dtlConfig.js
+++ app/core/modules/dtlConfig/dtlConfig.js
@@ -7,7 +7,7 @@
 	angular.module('dtlConfig', [])
 		.constant("CONFIG", {
 			//TODO [DB] set these values from environment values
-			NO_SERVICE: true,
+			NO_SERVICE: false,
 			DEBUG: true,
 			IMG_ALT: '//placehold.it/128x192/f8f8f8&text=DTL'
 		});
