a4959d3c22f834e1f576bc98cd2d9c0fa3ce5e1d
dejan-babic <dejan.babic@devtechgroup.com>
2015-10-08 18:18:41 +0200

    tmp timeout for nicer looks

--- app/splashscreen/services/dtlDeviceManagerService.js
+++ app/splashscreen/services/dtlDeviceManagerService.js
@@ -7,9 +7,9 @@
 	angular.module('dtl').
 		factory('dtlDeviceManager', dtlDeviceManager);
 
-	dtlDeviceManager.$inject = ['$log', '$q', 'deviceDetector', 'SYSTEM_MESSAGE'];
+	dtlDeviceManager.$inject = ['$log', '$q', '$timeout', 'deviceDetector', 'SYSTEM_MESSAGE'];
 
-	function dtlDeviceManager($log, $q, deviceDetector, SYSTEM_MESSAGE) {
+	function dtlDeviceManager($log, $q, $timeout, deviceDetector, SYSTEM_MESSAGE) {
 
 		return {
 			checkDevice: checkDevice,
@@ -19,11 +19,14 @@
 		function checkDevice() {
 			$log.debug('dtlDeviceManager:checkDevice()');
 			var deferred = $q.defer();
-			if (deviceDetector.isDesktop()) {
-				deferred.resolve(SYSTEM_MESSAGE.ACTION_OK)
-			} else {
-				deferred.reject(SYSTEM_MESSAGE.DEVICE_NOT_SUPPORTED);
-			}
+
+			$timeout(function(){
+				if (deviceDetector.isDesktop()) {
+					deferred.resolve(SYSTEM_MESSAGE.ACTION_OK)
+				} else {
+					deferred.reject(SYSTEM_MESSAGE.DEVICE_NOT_SUPPORTED);
+				}
+			}, 2000);
 
 			return deferred.promise;
 		}
@@ -31,7 +34,11 @@
 		function checkUserAgent() {
 			$log.debug('dtlDeviceManager:checkUserAgent()');
 			var deferred = $q.defer();
-			deviceDetector.browser === "chrome" ? deferred.resolve(SYSTEM_MESSAGE.ACTION_OK) : deferred.reject(SYSTEM_MESSAGE.BROWSER_NOT_SUPPORTED);
+
+			$timeout(function(){
+				deviceDetector.browser === "chrome" ? deferred.resolve(SYSTEM_MESSAGE.ACTION_OK) : deferred.reject(SYSTEM_MESSAGE.BROWSER_NOT_SUPPORTED);
+			}, 2000);
+
 			return deferred.promise;
 		}
 	}
